df1f31b43d00ed0ee732ea3a8f9b8848e095fd97
Mohammed Fawzaan <fawzaanmd31@gmail.com>
2026-04-21 00:04:13 +0530

    fixed pdf upload.

--- server/server.js
+++ server/server.js
@@ -1,5 +1,10 @@
 import http from "http";
+import fs from "fs";
 import app from "./app.js";
+
+if (!fs.existsSync("./uploads")) {
+    fs.mkdirSync("./uploads");
+}
 
 const server = http.createServer(app);
 
